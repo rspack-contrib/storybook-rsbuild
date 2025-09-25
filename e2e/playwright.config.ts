562092819950339a3eebad8b47d94345c5be73ff
fi3ework <fi3ework@gmail.com>
2025-09-26 01:11:40 +0800

    v2.1.2

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,10 +4,8 @@ const isCI = !!process.env.CI
 
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: false,
   forbidOnly: isCI,
   retries: isCI ? 1 : 0,
-  workers: 1,
   reporter: [['list']],
   timeout: 180_000,
   use: {
