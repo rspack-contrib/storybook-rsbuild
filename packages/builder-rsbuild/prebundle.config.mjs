95443419e940a94343512d524f84dd6d7a4db848
Wei <fi3ework@gmail.com>
2025-11-15 18:04:39 +0800

    fix: prebundle @storybook/core-webpack (#382)
    * fix: prebundle @storybook/core-webpack
    
    * fix(builder-rsbuild): fallback when core-webpack prebundle missing
    
    * fix(builder-rsbuild): prebundle during prep

---
+++ packages/builder-rsbuild/prebundle.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('prebundle').Config} */
+const config = {
+  prettier: true,
+  externals: {
+    'storybook/internal/common': 'storybook/internal/common',
+    'storybook/internal/node-logger': 'storybook/internal/node-logger',
+  },
+  dependencies: [
+    {
+      name: '@storybook/core-webpack',
+    },
+  ],
+}
+
+export default config
